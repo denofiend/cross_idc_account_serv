b1154af1eb310b30fd51316ddf34223a0501866a
denofiend <denofiend@gmail.com>
2013-01-10 20:34:40 +0800

    add sh in center queue script

---
+++ user_api_center_serv/queue/sync.sh
@@ -0,0 +1,10 @@
+#########################################################################
+# File Name: sync.sh
+# Author: DenoFiend
+# mail: denofiend@gmailcom
+# Created Time: 2013年01月10日 星期四 20时33分49秒
+#########################################################################
+#!/bin/bash
+
+python user_api_queue_center.py user-api-local.maxthon.cn 1986 user-api-local.maxthon.com 80
+
